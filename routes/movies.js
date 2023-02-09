6f44ac91e32f88f829a2bf96fbdf2e6661cbc5e9
Roman B <onwork.r@yandex.ru>
2023-02-10 00:52:07 +0300

    описал валидацию в отдельном модуле

--- routes/movies.js
+++ routes/movies.js
@@ -1,52 +1,14 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate'); // Валидация приходящих на сервер данных
-const isURL = require('validator/lib/isURL');// валидация URL
 const { getMovies, createMovies, deleteMovies } = require('../controllers/movies'); // импорт контроллеров
-Joi.objectId = require('joi-objectid')(Joi); // Пакет для валидации id
+const { customerCreateValidator, customerDeleteValidator } = require('../middlewares/customerValidator');// импорт мидлвары с celebrate
 
 // сработает при GET-запросе на URL '/movies' - возвращает все фильмы
 router.get('/', getMovies);
 
 // сработает при POST-запросе на URL '/movies' - добавляет фильм
-router.post('/', celebrate({
-  body: Joi.object().keys({
-    country: Joi.string().required().min(2).max(30),
-    director: Joi.string().required().min(1).max(30),
-    duration: Joi.number().required().min(2).max(999),
-    year: Joi.number().required().min(2).max(2090),
-    description: Joi.string().required().min(2).max(999),
-    image: Joi.string().required()
-      .custom((value, helpers) => {
-        if (isURL(value)) {
-          return value;
-        }
-        return helpers.message('Введен некорректный URL');
-      }), // кастомная валидация
-    trailerLink: Joi.string().required()
-      .custom((value, helpers) => {
-        if (isURL(value)) {
-          return value;
-        }
-        return helpers.message('Введен некорректный URL');
-      }), // кастомная валидация,
-    thumbnail: Joi.string().required()
-      .custom((value, helpers) => {
-        if (isURL(value)) {
-          return value;
-        }
-        return helpers.message('Введен некорректный URL');
-      }), // кастомная валидация,
-    movieId: Joi.number().required().min(1).max(999),
-    nameRU: Joi.string().required().min(2).max(30),
-    nameEN: Joi.string().required().min(2).max(30),
-  }),
-}), createMovies);
+router.post('/', customerCreateValidator, createMovies);
 
 // сработает при DELETE-запросе на URL '/movies/:_id' - удаляет карточку по идентификатору
-router.delete('/:_id', celebrate({
-  params: Joi.object().keys({
-    _id: Joi.objectId(), // валидация с помощью пакета "joi-objectId"
-  }),
-}), deleteMovies);
+router.delete('/:_id', customerDeleteValidator, deleteMovies);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,12 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate'); // Валидация приходящих на сервер данных
 const { getCurrentUser, updateProfile } = require('../controllers/users');// импорт контроллеров
+const { customerPatchValidator } = require('../middlewares/customerValidator');// импорт мидлвары с celebrate
 
 // eslint-disable-next-line max-len
 // сработает при GET-запросе на URL '/users/me' - получить информацию о текущем пользователе (email и имя)
 router.get('/me', getCurrentUser);
 
 // сработает при PATCH-запросе на URL '/users/me' - обновляет профиль(email и имя)
-router.patch('/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
-  }),
-}), updateProfile);
+router.patch('/me', customerPatchValidator, updateProfile);
 
 module.exports = router;
